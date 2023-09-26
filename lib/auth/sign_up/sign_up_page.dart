803bfdd232f90016f8c25a9be5e24d23de8c1460
vishwanath nishad <om.vishwanath.91@gmail.com>
2023-09-26 19:41:18 +0530

    signUp form created work fine

---
+++ lib/auth/sign_up/sign_up_page.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/auth/sign_up/sign_up_function.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final _formkey = GlobalKey<FormState>();
+  String userName = '';
+  String email = "";
+  String password = "";
+
+  printValut() {
+    print(userName);
+    print(email);
+    print(password);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("SignUp Form"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(30),
+            child: Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      key: const ValueKey("User Name"),
+                      decoration: const InputDecoration(hintText: "User Name"),
+                      validator: (value) {
+                        if (value.toString().isEmpty) {
+                          return "User Name should not be empty";
+                        } else if (value.toString().length < 3) {
+                          return "User Name should Not be Less them 3";
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        setState(() {
+                          userName = newValue.toString();
+                        });
+                      },
+                    ),
+                    TextFormField(
+                      key: const ValueKey("email"),
+                      decoration: const InputDecoration(
+                          hintText: "Enter your Email Here"),
+                      validator: (value) {
+                        if (value.toString().isEmpty) {
+                          return "email should not be empty";
+                        } else if (!(value.toString().contains("@"))) {
+                          return "invalid email address";
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        setState(() {
+                          email = newValue.toString();
+                        });
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      key: const ValueKey("password"),
+                      decoration: const InputDecoration(
+                          hintText: "Enter your password Here"),
+                      validator: (value) {
+                        if (value.toString().isEmpty) {
+                          return "password should not be empty";
+                        } else if (value.toString().length < 7) {
+                          return "password should not be less then 7";
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        setState(() {
+                          password = newValue.toString();
+                        });
+                      },
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          if (_formkey.currentState!.validate()) {
+                            _formkey.currentState!.save();
+                            createUserWithEmailAndPassword(email, password);
+                            printValut();
+                          }
+                        },
+                        child: const Text("SignUp"))
+                  ],
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
